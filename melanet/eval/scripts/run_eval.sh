bf72479af2c8d24e7adc40c50ad64e9cf795a94a
sulcm <sulcmatej@seznam.cz>
2026-03-05 01:26:48 +0100

    testing eval on metacentrum

--- melanet/eval/scripts/run_eval.sh
+++ melanet/eval/scripts/run_eval.sh
@@ -1,10 +1,10 @@
 python run_eval.py \
     --classification_task "multiclass" \
-    --model_name_or_path "/home/sulcm/models/melanet/caformer_s18/melanet_caformer_s18_ce_reg_aug-y7h03asy" \
-    --dataset_name "/home/sulcm/datasets/milk10k/TestMILK10k" \
+    --model_name_or_path "/storage/plzen4-ntis/home/sulcm01/outputs/swinv2_base/melanet_swinv2_base_ce_reg_aug-38iimmkp" \
+    --dataset_name "metacentrum_scratch/TestMILK10k" \
     --eval_split "test" \
     --results_path "./outputs/intermediate.json" \
     --batch_size 128 \
     --id_column_name "lesion_id" \
-    --isic_submission_path "./outputs/caformer_s18_y7h03asy_baseline.csv" \
+    --isic_submission_path "./outputs/swinv2_base_38iimmkp_baseline.csv" \
     # --label_column_name "label" \
