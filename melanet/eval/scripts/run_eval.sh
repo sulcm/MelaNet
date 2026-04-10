73f26e7b8f1ce9a1d7a635964721df668956af3d
sulcm <sulcmatej@seznam.cz>
2026-04-10 16:20:12 +0200

    finishing current set of ablations, adding pred resolution strategy to classifier and testing

--- melanet/eval/scripts/run_eval.sh
+++ melanet/eval/scripts/run_eval.sh
@@ -1,10 +1,11 @@
 python run_eval.py \
     --classification_task "multiclass" \
-    --model_name_or_path "/home/sulcm/models/melanet/vit_base/melanet_vit_base_grid-xs5p4lv0" \
+    --model_name_or_path "/home/sulcm/models/melanet/resnet50/melanet_resnet50_fine_grid-n5511yzn" \
+    --prediction_resolution_strategy "mean" \
     --dataset_name "/home/sulcm/datasets/milk10k/TestMILK10k" \
     --eval_split "test" \
     --results_path "./outputs/intermediate.json" \
     --batch_size 128 \
     --id_column_name "lesion_id" \
-    --isic_submission_path "./outputs/vit_base_xs5p4lv0_grid.csv" \
+    --isic_submission_path "./outputs/resnet50_n5511yzn_mean.csv" \
     # --label_column_name "label" \
