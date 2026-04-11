f32772051a38eb807f1bbf6a4397b6645fab0b11
sulcm <sulcmatej@seznam.cz>
2026-04-11 18:22:50 +0200

    creating new submissions for MILK10k with different prediction resolution strategies

--- melanet/eval/scripts/run_eval.sh
+++ melanet/eval/scripts/run_eval.sh
@@ -1,11 +1,11 @@
 python run_eval.py \
     --classification_task "multiclass" \
-    --model_name_or_path "/home/sulcm/models/melanet/resnet50/melanet_resnet50_fine_grid-n5511yzn" \
-    --prediction_resolution_strategy "mean" \
+    --model_name_or_path "/home/sulcm/models/melanet/vit_base/melanet_vit_base_fine_grid-dxjjru4m" \
+    --prediction_resolution_strategy "greedy" \
     --dataset_name "/home/sulcm/datasets/milk10k/TestMILK10k" \
     --eval_split "test" \
     --results_path "./outputs/intermediate.json" \
     --batch_size 128 \
     --id_column_name "lesion_id" \
-    --isic_submission_path "./outputs/resnet50_n5511yzn_mean.csv" \
+    --isic_submission_path "./outputs/vit_base_dxjjru4m_greedy.csv" \
     # --label_column_name "label" \
